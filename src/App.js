f75f0aa327ba1076d6725eb7c4a70d1def82ec64
KalebLight <kaleb.luiz00@gmail.com>
2023-08-21 23:17:55 -0300

    edit msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Se tudo der certo, esse vai ser o site da gente
         </p>
         <a
           className="App-link"
