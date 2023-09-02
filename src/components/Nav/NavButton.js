0bd3fe516618661f0c36859fd13032c3cb4e4e46
KalebLight <kaleb.luiz00@gmail.com>
2023-09-02 11:55:00 -0300

    deleting everything

--- src/components/Nav/NavButton.js
+++
@@ -1,9 +0,0 @@
-export function NavButton({ text }) {
-  return (
-    <button className="navButton">
-      <p className="py-2 px-4 rounded-md text-sm text-white">{text}</p>
-    </button>
-  );
-}
-
-export default NavButton;
